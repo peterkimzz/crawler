48a6d88f4dc102b6295d7349d9920881b7406acc
Peterkimzz <peterkimzz69@gmail.com>
2018-09-01 08:20:52 +0900

    Parse datas

--- src/naver/map.py
+++ src/naver/map.py
@@ -7,7 +7,7 @@ from time import sleep
 
 def setupMysql():
 
-    conn = pymysql.connect(host='rds-peter.ciyd9dirrvx6.ap-northeast-2.rds.amazonaws.com',
+    conn = pymysql.connect(host='127.0.0.1',
                            user='root',
                            password='Dhrla153186!',
                            db='grooming')
@@ -25,7 +25,9 @@ def init():
     driver = setupDriver()
     driver.get('https://map.naver.com')
 
-    driver.find_element_by_xpath('//*[@id="search-input"]').send_keys('강남 미용')
+    search_term = '강남 미용'
+    driver.find_element_by_xpath(
+        '//*[@id="search-input"]').send_keys(search_term)
     driver.find_element_by_xpath(
         '//*[@id="header"]/div[1]/fieldset/button').click()
 
@@ -33,9 +35,18 @@ def init():
     soup = BeautifulSoup(html, 'html.parser')
     list = soup.select(
         '#panel > div.panel_content.nano.has-scrollbar > div.scroll_pane.content > div.panel_content_flexible > div.search_result > ul > li')
-    for i, item in enumerate(list):
-        print(i)
-        print(item.get('src'))
+    for item in list:
+
+        title = item.find('a').get_text()
+        roadAddress = item.find('dd', class_="addr").contents[0].strip()
+        telephone = item.find('dd', class_="tel").string.strip()
+        category = item.find('dd', class_="cate").string.strip()
+        img_url = item.find('img').get('src')
+        print(title)
+        print(roadAddress)
+        print(telephone)
+        print(category)
+        print(img_url)
 
 
 if __name__ == '__main__':
