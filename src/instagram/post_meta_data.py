7f497d72d25526177537f63895f6aa1b1d79ebaa
Peter Kim <peterkimzz69@gmail.com>
2018-09-18 22:05:20 +0900

    Edit modules

--- src/instagram/post_meta_data.py
+++ src/instagram/post_meta_data.py
@@ -4,7 +4,8 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 # from modules.SeleniumDriver import selenium
-from modules.Slack.slack import Slack
+# from modules.Slack.slack import Slack
+from modules.slack import Slack
 
 if __name__ == '__main__':
 
