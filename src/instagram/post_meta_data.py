30735f4591783cdf70f6d57dffd05fbb389095a7
Peter Kim <peterkimzz69@gmail.com>
2018-09-18 21:54:41 +0900

    Fixed slack package

--- src/instagram/post_meta_data.py
+++ src/instagram/post_meta_data.py
@@ -3,13 +3,14 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from modules.SeleniumDriver import selenium
-from modules.Slack import slack
+# from modules.SeleniumDriver import selenium
+from modules.Slack.slack import Slack
 
 if __name__ == '__main__':
 
-    ss = selenium.Selenium()
-    print(ss)
+    # ss = selenium.Selenium()
+    # print(ss)
 
-    slack = slack.Slack()
-    print(slack)
+    slack = Slack()
+    slack.send_message('qwf')
+    # slack.chat.post_message('#notifiactions', 'qwe')
--- src/modules/Slack/slack.py
+++ src/modules/Slack/slack.py
@@ -6,10 +6,10 @@ class Slack():
 
         token = 'xoxb-363734903206-431926758755-BO6XrPDrPlx4pHz6Onc13ZrZ'
         slack = Slacker(token)
-        print('Slacker is created.')
-        self = slack
 
-    def send_message(text=None, channel='#notifications', attachments=None):
+        self.slack = slack
 
-        self.chat.post_message(text=text, channel=channel,
-                               attachments=attachments)
+    def send_message(self, text=None, channel='#notifications', attachments=None):
+
+        self.slack.chat.post_message(
+            text=text, channel=channel, attachments=attachments)
