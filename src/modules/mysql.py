1fb1d9c38400818ed88635daf43980235ad6a937
peterkimzz <peterkimzz69@gmail.com>
2018-10-03 23:33:50 +0900

    test selenium

--- src/modules/mysql.py
+++ src/modules/mysql.py
@@ -3,8 +3,27 @@ import pymysql
 
 class Mysql():
     def __init__(self):
-        conn = pymysql.connect(host='rds-maria.ciyd9dirrvx6.ap-northeast-2.rds.amazonaws.com',
-                               user='root',
-                               password='Dhrla153186!',
-                               db='grooming')
+
+        host = "rds-maria.ciyd9dirrvx6.ap-northeast-2.rds.amazonaws.com"
+        user = "root"
+        password = "Dhrla153186!"
+        db = "beauty_scandal"
+
+        conn = pymysql.connect(host=host,
+                               user=user,
+                               password=password,
+                               db=db)
+
+        print("MySQL Host %s is connected." % host)
+
+        self.pymysql = pymysql
         self.conn = conn
+
+    def select(self, sql):
+        try:
+            with self.conn.cursor(self.pymysql.cursors.DictCursor) as cursor:
+                cursor.execute(sql)
+                rows = cursor.fetchall()
+                return rows
+        except:
+            print("MySQL select query get error.")
